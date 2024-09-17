f8f7af376af03d6c4399b5eb33af292015188403
Kolesnichenko0 <den.koleskos@gmail.com>
2024-09-18 01:18:46 +0300

    Changed task04

--- JavaWebAppsKDELab14/src/main/java/csit/semit/kde/javawebappskdelab14/servlets/CheckLoginServlet.java
+++ JavaWebAppsKDELab14/src/main/java/csit/semit/kde/javawebappskdelab14/servlets/CheckLoginServlet.java
@@ -90,3 +90,87 @@ public class CheckLoginServlet extends HttpServlet {
         response.getWriter().write(jsonResponse.toString());
     }
 }
+
+
+
+
+
+
+
+
+//package csit.semit.kde.javawebappskdelab14.servlets;
+//
+//        import jakarta.servlet.ServletException;
+//        import jakarta.servlet.annotation.WebServlet;
+//        import jakarta.servlet.http.HttpServlet;
+//        import jakarta.servlet.http.HttpServletRequest;
+//        import jakarta.servlet.http.HttpServletResponse;
+//
+//        import java.io.IOException;
+//
+///**
+// * The {@code LoginServlet} class handles the login functionality of the web application.
+// * It extends the {@link HttpServlet} class, and overrides the {@code doGet} and {@code doPost} methods to handle GET and POST requests respectively.
+// * <p>
+// * The servlet is mapped to the "/check-login" URL pattern.
+// * <p>
+// * The {@code LoginServlet} class uses a hardcoded username and password for validation. If the username and password entered by the user match the hardcoded values, the user is redirected to the main page. Otherwise, an error message is displayed.
+// * <p>
+// * The class also provides an {@code init} method to initialize the path to the main page.
+// * <p>
+// *
+// * @author Kolesnychenko Denys Yevhenovych CS-222a
+// * @see HttpServlet
+// * @see HttpServletRequest
+// * @see HttpServletResponse
+// * @since 1.0.0
+// */
+//@WebServlet("/check-login")
+//public class CheckLoginServlet extends HttpServlet {
+//    private static final String LOGIN_JSP_PATH = "/jsp/login.jsp";
+//    private static final String VALID_USERNAME = "user";
+//    private static final String VALID_PASSWORD = "123456";
+//
+//    private String mainPath;
+//    /**
+//     * Initializes the servlet. This method is called by the servlet container before the servlet can service any request.
+//     * It sets the path to the main page of the web application.
+//     */
+//    @Override
+//    public void init() {
+//        mainPath = getServletContext().getContextPath() + "/main";
+//    }
+//    /**
+//     * Handles the HTTP GET method.
+//     * It forwards the request and response objects to the login page.
+//     *
+//     * @param request  the request object that contains client request data
+//     * @param response the response object to assist in sending a response to the client
+//     * @throws ServletException if an input or output error is detected when the servlet handles the GET request
+//     * @throws IOException      if the request for the GET could not be handled
+//     */
+//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.getRequestDispatcher(LOGIN_JSP_PATH).forward(request, response);
+//    }
+//
+//    /**
+//     * Handles the HTTP POST method.
+//     * It validates the username and password entered by the user. If they are valid, it redirects the user to the main page. Otherwise, it displays an error message.
+//     *
+//     * @param request  the request object that contains client request data
+//     * @param response the response object to assist in sending a response to the client
+//     * @throws ServletException if an input or output error is detected when the servlet handles the POST request
+//     * @throws IOException      if the request for the POST could not be handled
+//     */
+//    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//
+//        if (username.equals(VALID_USERNAME) && password.equals(VALID_PASSWORD)) {
+//            response.sendRedirect(mainPath);
+//        } else {
+//            request.setAttribute("error", "Invalid username or password");
+//            request.getRequestDispatcher(LOGIN_JSP_PATH).forward(request, response);
+//        }
+//    }
+//}
